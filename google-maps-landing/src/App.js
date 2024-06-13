c9fbd23f394bdc69c7d70280a9a7fc926822b31d
erickloutse <erickloutse@gmail.com>
2024-06-13 11:27:40 +0000

    Correction API Google Maps

--- google-maps-landing/src/App.js
+++ google-maps-landing/src/App.js
@@ -1,39 +1,31 @@
-import React from "react"; // Importation du module React depuis la bibliothèque 'react'
-import { GoogleMap, LoadScript } from "@react-google-maps/api"; // Importation des composants GoogleMap et LoadScript depuis '@react-google-maps/api'
-import "./App.css"; // Importation du fichier de style CSS './App.css'
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import "./App.css";
 
-// Définition du style de la carte Google Maps
 const mapStyles = {
-  height: "100%", // Hauteur de la carte à 100% de son conteneur
-  width: "100%", // Largeur de la carte à 100% de son conteneur
+  height: "100%",
+  width: "100%",
 };
 
-// Définition du centre par défaut de la carte (San Francisco, par exemple)
 const defaultCenter = {
-  lat: 37.7749, // Latitude
-  lng: -122.4194, // Longitude
+  lat: 14.7167, // Latitude de Dakar
+  lng: -17.4677, // Longitude de Dakar
 };
 
-// Définition du composant principal de l'application React
 function App() {
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
   return (
     <div className="container">
-      {" "}
-      {/* Conteneur principal avec la classe CSS 'container' */}
       <header className="header">
         Google Maps: Un outil incroyable utilisé tous les jours !
-      </header>{" "}
-      {/* En-tête de l'application avec la classe CSS 'header' */}
+      </header>
       <div className="map-container">
-        {" "}
-        {/* Conteneur de la carte avec la classe CSS 'map-container' */}
-        <LoadScript googleMapsApiKey="AIzaSyDisPvQeSyRdIlNiExe7aRx8utztqtOPew">
-          {" "}
-          {/* Composant LoadScript pour charger l'API Google Maps avec votre clé API */}
-          <GoogleMap // Composant GoogleMap
-            mapContainerStyle={mapStyles} // Propriété pour définir le style de la carte
-            zoom={13} // Niveau de zoom initial de la carte
-            center={defaultCenter} // Centre de la carte initial
+        <LoadScript googleMapsApiKey={googleMapsApiKey}>
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={13}
+            center={defaultCenter}
           />
         </LoadScript>
       </div>
@@ -41,4 +33,4 @@ function App() {
   );
 }
 
-export default App; // Exportation du composant App comme composant par défaut de ce fichier
+export default App;
